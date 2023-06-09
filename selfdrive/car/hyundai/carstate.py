202bd067b17e1086ea32f76f727255396bbe3d93
Phr00t <phr00t@gmail.com>
2023-06-09 16:44:32 -0400

    allow more time to re-enable cruise

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -281,7 +281,7 @@ class CarState(CarStateBase):
     if ret.brakePressed or ret.gasPressed or self.clu_Vanz < 20:
       # dont re-enable cruise if are manually modifying speed, or we've dropped below cruise speed
       self.time_cruise_cancelled = datetime.datetime(2000, 10, 1, 1, 1, 1,0)
-    elif self.opkr_autoresume and not self.acc_active and (datetime.datetime.now() - self.time_cruise_cancelled).total_seconds() < 5.0:
+    elif self.opkr_autoresume and not self.acc_active and (datetime.datetime.now() - self.time_cruise_cancelled).total_seconds() < 6.5:
       ret.cruiseState.nonAdaptive = True 
 
     # opkr
