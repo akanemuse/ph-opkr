2fa0d05eada25fa0458c08b08eeea79ef58fce2c
Phr00t <phr00t@gmail.com>
2023-06-12 22:13:26 -0400

    trigger more significant braking sooner specifically for sharp turns

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -549,8 +549,10 @@ class CarController():
     desired_speed = max_speed_in_mph
 
     # if we are apporaching a turn, slow down in preparation
+    # also note how much of a speed difference we need for this turn
     vcurv_adj = 0.35 + (0.65 / (0.46 * vcurv + 1))
     desired_speed *= vcurv_adj
+    curve_speed_ratio = clu11_speed / desired_speed
 
     # is there a lead?
     if l0prob > 0.5 and clu11_speed > 5:
@@ -597,6 +599,10 @@ class CarController():
       slow_speed_factor = 1.7
       if CS.opkr_autoresume:
         slow_speed_factor = 1.5
+        # also have better pre-curve slowing
+        # this can trigger sooner than lead car slowing, because curve data is much less noisy
+        if curve_speed_ratio > 1.2:
+          desired_speed = 0
 
       # if we are going much faster than we want, disable cruise to trigger more intense regen braking
       if clu11_speed > desired_speed * slow_speed_factor:
