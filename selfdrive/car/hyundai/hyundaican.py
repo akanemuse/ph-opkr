e13c6fa41bdcd947e14d1f6539f8816a62185394
Phr00t <phr00t@gmail.com>
2023-06-14 13:12:02 -0400

    more cleanup

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -87,11 +87,6 @@ def create_cpress(packer, clu11, button):
   values["CF_Clu_CruiseSwState"] = button
   return packer.make_can_msg("CLU11", 0, values)
 
-def create_regen(packer, elect_gears, set_step):
-  values = elect_gears
-  values["Elect_Gear_Step"] = set_step
-  return packer.make_can_msg("ELECT_GEAR", 0, values)
-
 def create_lfahda_mfc(packer, enabled, hda_set_speed=0):
   values = {
     "LFA_Icon_State": 2 if enabled else 0,
